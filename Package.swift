7773adef8a05fc238cd8f23db128e399d2b0b785
Sven A. Schmidt <sas@finestructure.co>
2024-09-16 12:18:36 +0200

    Remove executable

--- Package.swift
+++ Package.swift
@@ -9,9 +9,6 @@ let package = Package(
         .package(url: "https://github.com/vapor/postgres-nio.git", from: "1.22.1")
     ],
     targets: [
-        .executableTarget(name: "pg-perf-regression", dependencies: [
-            .product(name: "PostgresNIO", package: "postgres-nio")
-        ]),
         .testTarget(name: "PerfTest", dependencies: [
             .product(name: "PostgresNIO", package: "postgres-nio")
         ])
